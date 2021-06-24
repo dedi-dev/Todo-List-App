12c83389e3b8d74e188a16dc7815ead58c110bcf
Dedi-edot <id.dedi81@gmail.com>
2021-06-24 11:40:53 +0700

    Add React-Redux and Redux

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import rootReducer from "./redux/reducers";
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </provider>,
+
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/redux/reducers/todo.js
@@ -0,0 +1,9 @@
+const init_state = {
+  todoList: [],
+  inputTodo: "",
+  todoCount: 0,
+};
+
+export default (state = init_state, action) => {
+  return state;
+};
